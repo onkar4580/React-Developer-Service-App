5e556860ffbd4742f647953f4ff15fb29a468056
Onkar Kumar <onkar4580@gmail.com>
2022-10-29 12:44:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>ReactDev</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://t.me/onkar_93" target={"blank"} className="fa fa-telegram">
+            
+          </a>
+          <a href="https://www.instagram.com/constantinople_0/" target={"blank"} className="fa fa-instagram">
+            
+          </a>
+          <a href="https://github.com/onkar4580" target={"blank"} className="fa fa-github">
+            
+          </a>
+          <a href="https://twitter.com/constantinopl_e" target={"blank"} className="fa fa-twitter">
+            
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
